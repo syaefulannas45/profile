0a9cc9289aeec446d5fc4cbb2ea2bb59901ee69c
Syaeful Annas <syaefulannas33@gmail.com>
2023-01-29 20:13:49 +0700

    update

--- public/static/js/javascript.js
+++ public/static/js/javascript.js
@@ -1,4 +1,6 @@
-// Navbar
+//Animte On Scroll
+
+// Navbar-Section
 let bar = document.getElementById("bar");
 let mark = document.getElementById("mark");
 function setToggle() {
@@ -6,39 +8,99 @@ function setToggle() {
   mark.classList.toggle("mark-active");
 }
 
-//About
-const getImage = [
+window.onscroll = function () {
+  const header = document.querySelector("#navbar");
+  const fixedNav = header.offsetTop;
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+//About-Section
+const imagesAbout = [
   {
-    image: "images/about/html.svg",
+    url: "images/about/ex.svg",
   },
   {
-    image: "images/about/css.svg",
+    url: "images/about/nodejs.svg",
   },
   {
-    image: "images/about/js.svg",
+    url: "images/about/mongodb.svg",
   },
   {
-    image: "images/about/php.svg",
+    url: "images/about/jquery.svg",
   },
   {
-    image: "images/about/mysql.svg",
+    url: "images/about/mysql.svg",
   },
   {
-    image: "images/about/jquery.svg",
+    url: "images/about/php.svg",
   },
   {
-    image: "images/about/mongodb.svg",
+    url: "images/about/js.svg",
   },
   {
-    image: "images/about/nodejs.svg",
+    url: "images/about/css.svg",
   },
   {
-    image: "images/about/ex.svg",
+    url: "images/about/html.svg",
   },
 ];
+
 let getAboutLeft = document.getElementById("aboutleft");
-const print = getImage.map((index) => {
-  return `<div class="bg-white w-[85px] h-[85px] rounded-full shadow-custom mt-[40px] flex justify-center items-center"><img src=${index.image} class="w-[45px] h-[45px]" /></div>`;
+function loopImageAbout(url) {
+  const image = `
+<div  class="bg-white w-[85px] h-[85px] rounded-full shadow-custom mt-[40px] flex justify-center items-center "><img src=${url} class="w-[45px] h-[45px]" /></div>
+`;
+  getAboutLeft.insertAdjacentHTML("afterbegin", image);
+}
+
+imagesAbout.forEach((image) => {
+  loopImageAbout(image.url);
 });
 
-getAboutLeft.insertAdjacentHTML("afterbegin", print);
+//Project-Section
+const imagesProject = [
+  {
+    url: "images/project/img-6.svg",
+    id: 6,
+  },
+  {
+    url: "images/project/img-5.svg",
+    id: 5,
+  },
+  {
+    url: "images/project/img-4.svg",
+    id: 4,
+  },
+  {
+    url: "images/project/img-3.svg",
+    id: 3,
+  },
+  {
+    url: "images/project/img-2.svg",
+    id: 2,
+  },
+  {
+    url: "images/project/img-1.svg",
+    id: 1,
+  },
+];
+
+let getProjectImg = document.getElementById("images-project");
+
+function loopImageProject(url, increment) {
+  const image = `<div id=${increment} class="cursor-pointer before:hover:content-['See_More'] before:hover:absolute  before:hover:text-white before:hover:bg-green-200 before:hover:py-[10px] before:hover:px-[10px] before:hover:rounded-[10px]"><img src=${url} alt="img" class="w-[400px] h-[300px] my-[10px] " /></div>`;
+
+  getProjectImg.insertAdjacentHTML("afterbegin", image);
+}
+
+imagesProject.forEach((image) => {
+  loopImageProject(image.url, image.id);
+  getProjectImg.addEventListener("click", () => {
+    const modal = `<div class="absolute">test</div>`;
+    getProjectImg.insertAdjacentHTML("afterbegin", modal);
+  });
+});
--- public/static/js/tailwind.config.js
+++ public/static/js/tailwind.config.js
@@ -6,9 +6,15 @@ tailwind.config = {
           100: "#15803D",
           200: "#166534",
         },
+        input: {
+          100: "#ECEBEBCC",
+          200: "#5F5353",
+        },
       },
       boxShadow: {
         custom: "1px 1px 1px 1px #d6d6d6",
+        navbar: "inset 0 -1px 0 0 rgba(0, 0, 0, 0.2)",
+        button: "3px 3px 6px 1px #AAA5A5",
       },
     },
   },

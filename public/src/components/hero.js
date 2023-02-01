19b6e75c3701a6e7f840f1842647fa4c804bc8d2
Syaeful Annas <syaefulannas33@gmail.com>
2023-02-01 12:04:35 +0700

    update

--- public/src/components/hero.js
+++ public/src/components/hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
               Nawasena Group
             </h1>
             <p class="
-           text-[12px] lg:text-[20px] leading-[30px] dark:text-slate-100">
+           text-[16px] lg:text-[20px] leading-[30px] dark:text-slate-100">
              Kami adalah startup yang berfokus pada bidang pengembangan teknologi website dan android
             </p>
             <button class="text-white px-[20px] lg:px-[25px] py-[7px] lg:py-[10px] mt-[28px] rounded-[6px] bg-gradient-to-r from-blues-100 to-blues-200 bg">
@@ -17,10 +17,10 @@ const Hero = () => {
           </div>
           <div class="lg:w-2/4 relative flex lg:justify-center pt-[50px] lg:pt-0">
             <img src="public/images/hero/hero.svg" alt="images" class="lg:h-[600px] px-12 relative lg:static z-10" />
-            <img src="public/images/hero/elips-1.svg" alt="images" class="absolute lg:left-[1px] lg:top-[220px] top-[140px] left-[10px] animate-bounce z-20" />
-            <img src="public/images/hero/elips-2.svg" alt="images" class="absolute lg:left-[-15px] lg:bottom-0 bottom-[-10px] lg:h-[110px] h-[70px] left-[18px] z-20" />
+            <img src="public/images/hero/elips-1.svg" alt="images" class="absolute lg:left-[50px] lg:top-[210px] top-[140px] left-[10px] animate-bounce z-20" />
+            <img src="public/images/hero/elips-2.svg" alt="images" class="absolute lg:left-[30px] lg:bottom-0 bottom-[-10px] lg:h-[110px] h-[70px] left-[18px] z-20" />
             <img src="public/images/hero/elips-3.svg" alt="images" class="absolute right-[-10px] top-[150px] lg:right-0 lg:top-[200px]" />
-            <img src="public/images/hero/ilustrasi.svg" alt="images" class="absolute lg:left-[-50px] lg:top-[50px] lg:h-[500px] lg:h-[500px] h-[300px] top-[70px] z-20" />
+            <img src="public/images/hero/ilustrasi.svg" alt="images" class="absolute lg:left-0 lg:top-[50px] lg:h-[500px] lg:h-[500px] h-[300px] top-[70px] z-20" />
           </div>
         </div>
         `;

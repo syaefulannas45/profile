e49a4edaae6bbfe2ca5a5395bd4701d63f696a4c
Syaeful Annas <100054932+syaefulannas45@users.noreply.github.com>
2023-02-04 01:59:19 +0700

    Add files via upload

--- public/src/components/about.js
+++ public/src/components/about.js
@@ -2,9 +2,9 @@ let mainAbout = document.getElementById("about");
 
 const About = () => {
   return `
-  <div class="relative pt-[100px] lg:pt-[100px] dark:bg-background-100 px-4 lg:px-[120px] w-full overflow-hidden pb-[340px] lg:pb-10">
-        <img loading="lazy" src="public/images/about/vector-1.svg" alt="vector" class="absolute lg:top-8 lg:left-0 origin-top-left top-[60px] left-0 z-40 mobile:h-[300px] hidden lg:block" />
-        <img loading="lazy" src="public/images/about/vector-3.svg" alt="vector" class="absolute lg:top-8 lg:left-0 origin-top-left top-[180px] left-0  mobile:h-[100px] block lg:hidden" />
+  <div class="pt-[10%] lg:pt-[100px] dark:bg-background-100 px-4 lg:px-[120px] w-full overflow-hidden pb-[340px] lg:pb-10">
+        <div class="relative">
+        <img loading="lazy" src="public/images/about/vector-1.svg" alt="vector" class="absolute lg:top-[0%] lg:left-[-9%] top-[9%] left-[-4%] z-40 mobile:h-[100px]" />
         <h1 class="lg:text-[48px] font-bold bg-gradient-to-r from-blues-100 to-blues-200 bg-clip-text text-transparent text-center text-[35px]">Nawasena Group</h1>
         <p class="text-slate-800 text-[15px] lg:text-[20px] text-center dark:text-slate-100">A Growing startup from STMIK IKMI Cirebon</p>
         <div class="w-full lg:flex pt-6 lg:pt-0 lg:mt-[80px] lg:items-center">
@@ -18,16 +18,16 @@ const About = () => {
 
               Teknologi yang kami gunakan:
             </p>
-            <div id="aboutleft" class="flex lg:space-x-[40px] px-[30px] lg:p-0 justify-evenly lg:justify-start flex-wrap w-full"></div>
-            <img loading="lazy" src="public/images/about/elips-1.svg" class="absolute lg:right-10 lg:bottom-0 bottom-[350px] right-[50px] hidden lg:block" />
-            <img loading="lazy" src="public/images/about/elips-2.svg" class="absolute lg:right-[-15px] lg:bottom-10 bottom-[380px] right-0 hidden lg:block" />
-            <img loading="lazy" src="public/images/about/elips-3.svg" class="lg:hidden absolute left-[-20px] top-[400px] hidden" />
+            <div id="aboutleft" class="flex lg:space-x-[7%] px-[10%] space-x-[3.5%] lg:p-0 justify-evenly lg:justify-start flex-wrap w-full"></div>
+            <img loading="lazy" src="public/images/about/elips-1.svg" class="absolute lg:right-[8%] lg:bottom-[0%] hidden lg:block" />
+            <img loading="lazy" src="public/images/about/elips-2.svg" class="absolute lg:right-[0%] lg:bottom-[6%] hidden lg:block" />
           </div>
           <div data-aos="fade-left" class="lg:w-2/4 lg:pb-[150px] flex justify-center relative w-full">
-            <img loading="lazy" src="public/images/about/about.svg" alt="bg" class="z-10 absolute h-[270px] lg:top-[-200px] lg:left-[100px]" />
+            <img loading="lazy" src="public/images/about/about.svg" alt="bg" class="z-10 absolute h-[270px] lg:bottom-[50%] lg:left-[20%]" />
 
-            <img loading="lazy" src="public/images/about/vector-2.svg" alt="vector" class="absolute lg:bottom-[-50px] lg:right-[-20px] animate-pulse bottom-[-290px] right-0 mobile:h-[200px]" />
+            <img loading="lazy" src="public/images/about/vector-2.svg" alt="vector" class="absolute lg:bottom-[-30%] lg:right-[-5%] animate-pulse bottom-[-250px] right-0 mobile:h-[200px]" />
           </div>
+        </div>
         </div>
       </div>
      `;

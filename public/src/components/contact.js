c7565391da9cbc062fa1f688c66dfc611c613d61
Syaeful Annas <syaefulannas33@gmail.com>
2023-02-05 12:39:59 +0700

    update

--- public/src/components/contact.js
+++ public/src/components/contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
   return `
      <div class="relative px-4 lg:px-[120px] lg:h-[420px] dark:bg-background-100 py-10">
      <img loading="lazy" src="public/images/contact/elips.svg" class="absolute lg:top-[-380px] lg:left-10 hover:animate-pulse top-[-5px] left-[128px]" />
-     <img loading="lazy" src="public/images/contact/vector-1.svg" class="absolute lg:top-[-280px] lg:right-8 hover:animate-pulse" />
+     <img loading="lazy" src="public/images/contact/vector-1.svg" class="absolute lg:top-[-280px] lg:right-8 hover:animate-pulse hidden lg:block" />
      <img loading="lazy" src="public/images/contact/vector-2.svg" class="absolute lg:top-[-300px] lg:left-0 bottom-[10px] lg:bottom-0  left-0 hover:animate-pulse mobile:h-[300px]" />
           <div
             class="w-full lg:flex lg:justify-between bg-white py-[50px] px-4 lg:px-[100px] border  lg:bottom-[365px] relative shadow-lg rounded-[8px] dark:bg-background-200 dark:border-0">

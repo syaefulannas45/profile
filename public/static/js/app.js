c475c6e10d92ab1da014480d8401c10ca608aa67
Syaeful Annas <syaefulannas33@gmail.com>
2023-02-05 12:16:54 +0700

    update

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,3 +1,37 @@
+const darkModeToggle = document.querySelector("#lamp-toggle");
+const darkModeToggleMobile = document.querySelector("#lamp-toggle-mobile");
+const themes = localStorage.getItem("themes");
+
+const setDarkMode = (state) => {
+  document.body.className = state;
+  darkModeToggle.checked = state === "dark";
+  darkModeToggleMobile.checked = state === "dark";
+};
+
+darkModeToggle.addEventListener("change", (e) => {
+  localStorage.setItem("themes", e.target.checked ? "dark" : "light");
+  setDarkMode(e.target.checked ? "dark" : "light");
+});
+darkModeToggleMobile.addEventListener("change", (e) => {
+  localStorage.setItem("themes", e.target.checked ? "dark" : "light");
+  setDarkMode(e.target.checked ? "dark" : "light");
+});
+
+const activateToggle = () => {
+  const activateAllToggle = () => {
+    darkModeToggle.checked = true;
+    darkModeToggleMobile.checked = true;
+  };
+  const unsetToggleActive = () => {
+    darkModeToggle.checked = false;
+    darkModeToggleMobile.checked = false;
+  };
+  themes == "dark" ? activateAllToggle() : unsetToggleActive();
+  document.body.classList.add(themes);
+};
+
+themes && themes !== null ? activateToggle() : document.body.classList.remove(themes);
+
 window.onscroll = function () {
   const header = document.querySelector("#fixed");
   const fixedNav = header.offsetTop;
@@ -10,17 +44,6 @@ window.onscroll = function () {
   }
 };
 
-const lampToggle = () => {
-  const lampToggle = document.querySelector("#lamp-toggle");
-  const lampMobile = document.querySelector("#lamp-toggle-mobile");
-  const html = document.querySelector("html");
-  if (lampToggle.checked || lampMobile.checked) {
-    html.classList.add("dark");
-  } else {
-    html.classList.remove("dark");
-  }
-};
-
 function alert() {
   swal("Succes", "Thanks for sending the message", "success");
 }
@@ -28,7 +51,4 @@ function email() {
   swal({
     text: "Kirimkan sebuah pesan email ke : official.adcom@gmail.com",
   });
-}
-function sosmed() {
-  swal("Oops", "Mohon maaf akun belum dibuat", "error");
 }
